a0884ea9a899c56481e4e35335d52575fef11e35
service-city-panama <163794663+service-city-panama@users.noreply.github.com>
2024-04-17 21:59:13 -0400

    Exitoso registro con email y contraseña.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 import './login_Google.js'
-import '../app/firebase.js'
+import '../app/signupForm.js'
 
 
-// ========== ========== NavBar ========== ==========
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
 
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
 
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
-// ========== ========== NavBar ========== ==========
+
+
+
+
+
+
+// // ========== ========== NavBar ========== ==========
+// const nav = document.querySelector("#nav");
+// const abrir = document.querySelector("#abrir");
+// const cerrar = document.querySelector("#cerrar");
+
+// abrir.addEventListener("click", () => {
+//     nav.classList.add("visible");
+// })
+
+// cerrar.addEventListener("click", () => {
+//     nav.classList.remove("visible");
+// })
+// // ========== ========== NavBar ========== ==========
 
