8441ef05909601592f4cc7dc30e5e848e21f792e
service-city-panama <163794663+service-city-panama@users.noreply.github.com>
2024-04-17 22:56:55 -0400

    Add files via upload

--- app/signupForm.js
+++ app/signupForm.js
@@ -1,5 +1,6 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import { auth } from "./firebase.js";
+import { showMessage } from "./showMessage.js";
 
 const signupForm = document.querySelector("#signupForm");
 
@@ -21,19 +22,20 @@ signupForm.addEventListener("submit", async (e) => {
     const modal = bootstrap.Modal.getInstance(signupModal);
     modal.hide();
 
+showMessage(userCredentials.user.email + " ¡Ha sido creado exitósamente!")
   } catch (error) {
     if (error.code === "auth/email-already-in-use") {
-      alert("¡Este correo ya está en uso!, intente iniciar sesión o crear otro distinto a este.");
+      showMessage("¡Este correo ya está en uso!, intente iniciar sesión o crear otro distinto a este.");
     } else if (error.code === "auth/invalid-email") {
-      alert(
+      showMessage(
         "¡Correo inválido!, verifique que esté bien escrito e intente nuevamente."
       );
     } else if (error.code === "auth/weak-password") {
-      alert(
+      showMessage(
         "¡Esta contraseña es corta y/o insegura!, se recomienda tener 8 caracteres o más, de lo posible combíne letras minúscula con mayúscula y número."
       );
     } else if (error.code) {
-      alert("¡Algo a salido mal!, no logro detectar el error.");
+      showMessage("¡Algo a salido mal!, no logro detectar el error.");
     }
   }
 });
